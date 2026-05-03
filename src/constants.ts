5e548069093d502cd9206db1fc12e01a4cbe4728
Awais Amjad <awais@awais.me>
2026-05-03 17:33:55 +0100

    changes

--- src/constants.ts
+++ src/constants.ts
@@ -18,26 +18,20 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
-    name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    name: "Codeberg",
+    href: "https://codeberg.org/awais/astro-paper",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/awais-amjad-dev/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:awais@awais.me",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
